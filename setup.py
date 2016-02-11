0ad4f032d529392195f5a5faec2559db88f7dbfe
Brad Safnuk <brad.safnuk@cmich.edu>
2016-02-10 22:06:11 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='todone',
+      version='0.01',
+      description='Todo manager',
+      url='http://bitbucket.org/safnuk/todone',
+      author='Brad Safnuk',
+      author_email='safnuk@gmail.com',
+      license='MIT',
+      packages=['todone'],
+      zip_safe=False)
