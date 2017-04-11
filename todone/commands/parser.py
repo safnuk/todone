37e4d008a55105f5f93b5ade919e70135639fa44
Brad Safnuk <brad.safnuk@cmich.edu>
2017-04-10 23:57:15 -0400

    Dispatch error and help commands on parse error

--- todone/commands/parser.py
+++ todone/commands/parser.py
@@ -10,6 +10,7 @@ class Parser:
     def __init__(self, args):
         self._store_args(args)
         self._setup_parser()
+        self._commands = []
 
     def _store_args(self, args):
         if args:
@@ -23,7 +24,7 @@ class Parser:
         parser_initialization = [
             (factory.PresetArgument.config, {'name': 'config'}),
             (factory.PresetArgument.required_switch,
-             {'name': 'command', 'options': ALLOWED_COMMANDS}),
+             {'name': 'command', 'options': _ALLOWED_COMMANDS}),
             (factory.PresetArgument.all_remaining_passthrough,
              {'name': 'args'}),
         ]
@@ -31,13 +32,63 @@ class Parser:
 
     @property
     def commands(self):
+        if self._commands:
+            return self._commands
         try:
             self.parser.parse(self.args)
+            self._build_config_command()
+            self._build_parsed_command()
+            return self._commands
         except pe.ArgumentError:
-            return [{'command': 'error', 'message': 'Invalid argument(s)'}]
+            return [{'command': 'error', 'message': 'Invalid argument(s)'},
+                    {'command': 'help', 'short': True}]
+
+    def _build_config_command(self):
+        cmd = {'command': 'configure',
+               'file': self.parser.parsed_data['config']}
+        self._commands.append(cmd)
+
+    def _build_parsed_command(self):
+        arg_parser = ArgParser(self.parser.parsed_data['command'],
+                               self.parser.parsed_data['args'])
+        self._commands = self._commands + arg_parser.commands
 
 
-ALLOWED_COMMANDS = {
+class ArgParser:
+    def __init__(self, command, args):
+        self.parser = factory.ParserFactory.from_arg_list(
+            self._parser_init(command)
+        )
+        self._args = args
+        self._command = {'command': command}
+
+    @property
+    def commands(self):
+        try:
+            self.parser.parse(self._args)
+            self._command.update(self.parser.parsed_data)
+        except pe.ArgumentError as e:
+            command = self._command['command']
+            return [{'command': 'error',
+                     'message': 'Invalid argument(s) for {} command. {}'
+                     .format(command, e)},
+                    {'command': 'help', 'short': True, 'arg': command}]
+        return [self._command]
+
+    def _parser_init(self, command):
+        init = {
+            'help': {},
+            'version': {},
+            'folder': {},
+            'list': {},
+            'move': {},
+            'new': {},
+            'setup': {},
+            'done': {},
+        }
+        return init[command]
+
+_ALLOWED_COMMANDS = {
     '-h',
     '--help',
     'help',
--- todone/commands/tests/test_parser.py
+++ todone/commands/tests/test_parser.py
@@ -1,11 +1,19 @@
 from unittest import TestCase
 from unittest.mock import Mock, patch
 
+from hamcrest import assert_that, contains_string
 from todone.commands import parser
 from todone.parser import exceptions as pe
 
 
 class TestParser(TestCase):
+    def setUp(self):
+        self.mock_parsed_data = {
+            'command': 'command',
+            'args': ['args'],
+            'config': 'config.ini',
+        }
+
     def test_init_stores_passed_args(self):
         test_args = ['test', 'foo']
         cd = parser.Parser(test_args)
@@ -23,20 +31,68 @@ class TestParser(TestCase):
             cd = parser.Parser(None)
             self.assertEqual(cd.args, cd.DEFAULT_ARGS)
 
-    @patch('todone.commands.dispatch.factory.ParserFactory.from_arg_list')
-    def test_get_commands_with_ArgumentError_returns_error_command(
+    @patch('todone.commands.parser.factory.ParserFactory.from_arg_list')
+    def test_parsing_command_with_ArgumentError_returns_error_command(
             self, MockParser):
         mock_instance = MockParser.return_value
         mock_instance.parse = Mock(side_effect=pe.ArgumentError)
         p = parser.Parser(['garbage', 'args'])
         cmds = p.commands
-        self.assertEqual(len(cmds), 1)
         cmd = cmds[0]
         self.assertEqual(cmd['command'], 'error')
-        self.assertEqual(cmd['message'], 'Could not parse command')
+        self.assertEqual(cmd['message'], 'Invalid argument(s)')
+
+    @patch('todone.commands.parser.factory.ParserFactory.from_arg_list')
+    def test_parsing_command_with_ArgumentError_returns_help_command(
+            self, MockParser):
+        mock_instance = MockParser.return_value
+        mock_instance.parse = Mock(side_effect=pe.ArgumentError)
+        p = parser.Parser(['garbage', 'args'])
+        cmds = p.commands
+        cmd = cmds[1]
+        self.assertEqual(cmd['command'], 'help')
+        self.assertTrue(cmd['short'])
 
     def test_config_flag_parses_with_passed_file(self):
-        p = parser.Parser(['-c', 'foo'])
+        p = parser.Parser(['-c', 'foo', 'help'])
         cmd = p.commands[0]
         self.assertEqual(cmd['command'], 'configure')
         self.assertEqual(cmd['file'], 'foo')
+
+    def test_no_config_flag_parses_to_empty_config_file(self):
+        p = parser.Parser(['help'])
+        cmd = p.commands[0]
+        self.assertEqual(cmd['command'], 'configure')
+        self.assertEqual(cmd['file'], '')
+
+    @patch('todone.commands.parser.ArgParser')
+    def test_parsed_command_added_to_command_list(self, MockParser):
+        mock_instance = MockParser.return_value
+        mock_instance.command = self.mock_parsed_data
+        p = parser.Parser(['help'])
+        cmd = p.commands[1]
+        for k, v in cmd.items():
+            self.assertEqual(v, self.mock_parsed_data[k])
+
+    @patch('todone.commands.parser.factory.ParserFactory.from_arg_list')
+    def test_parsing_args_with_ArgumentError_returns_error_command(
+            self, MockTextParser):
+        mock_instance = MockTextParser.return_value
+        mock_instance.parse = Mock(side_effect=pe.ArgumentError)
+        p = parser.ArgParser('folder', ['garbage'])
+        cmds = p.commands
+        cmd = cmds[0]
+        self.assertEqual(cmd['command'], 'error')
+        assert_that(cmd['message'], contains_string('Invalid argument(s)'))
+
+    @patch('todone.commands.parser.factory.ParserFactory.from_arg_list')
+    def test_parsing_args_with_ArgumentError_returns_error_command(
+            self, MockTextParser):
+        mock_instance = MockTextParser.return_value
+        mock_instance.parse = Mock(side_effect=pe.ArgumentError)
+        p = parser.ArgParser('folder', ['garbage'])
+        cmds = p.commands
+        cmd = cmds[1]
+        self.assertEqual(cmd['command'], 'help')
+        self.assertTrue(cmd['short'])
+        self.assertEqual(cmd['arg'], 'folder')
